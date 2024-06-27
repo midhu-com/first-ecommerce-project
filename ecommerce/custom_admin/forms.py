39f31757bf0f04d1ce4057175fec51a06d2ae864
midhu-com <152254632+midhu-com@users.noreply.github.com>
2024-06-27 19:50:54 +0530

    Update forms.py

--- ecommerce/custom_admin/forms.py
+++ ecommerce/custom_admin/forms.py
@@ -1,4 +1,4 @@
-    from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 from category.models import Category
