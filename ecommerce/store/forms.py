c06f133a95af7782e325a20d38cd54c2fc0f2176
midhu-com <152254632+midhu-com@users.noreply.github.com>
2024-06-27 17:09:45 +0530

    Update forms.py

--- ecommerce/store/forms.py
+++ ecommerce/store/forms.py
@@ -5,8 +5,21 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model=ReviewRating
         fields  =['subject','review','rating']
+class MultipleFileInput(forms.ClearableFileInput):
+    template_name = 'django/forms/widgets/clearable_multiple_file_input.html'
+    
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget']['attrs'].update({'multiple': True})
+        return context
 
 class ImageForm(forms.ModelForm):
+    images = forms.FileField(widget=MultipleFileInput(attrs={
+        'class': 'form-control-file',
+    }), label='Product Images', required=False)
+
+    cropped_images = forms.ImageField(widget=forms.HiddenInput(), required=False)
+    
     class Meta:
         model = Image
-        fields = '__all__'  # Adjust as needed based on desired fields
+        fields = ['images', 'cropped_image']
