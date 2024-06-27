30a9bb7d97b0b971244171df32fca1b2019a5a65
midhu-com <152254632+midhu-com@users.noreply.github.com>
2024-06-27 20:43:12 +0530

    Update views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -39,6 +39,13 @@ def store(request, category_slug=None):
         products = products.filter(variations__variation_value__iexact=selected_color, variations__variation_category__iexact='color')
     if selected_size:
         products = products.filter(variations__variation_value__iexact=selected_size, variations__variation_category__iexact='size')
+
+    for product in products:
+        total_stock = 0
+        variations = product.variations.filter(is_active=True)
+        for variation in variations:
+            total_stock += variation.stock
+        product.total_stock = total_stock  
     
     # Pagination
     paginator = Paginator(products, 8)
